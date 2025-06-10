41db106aeee1b40e49e39dc9a199ef6411aeadcb
DeyvisonTav <deyvisontav@gmail.com>
2025-06-10 17:29:28 -0300

    Add RabbitMQ service configuration in docker-compose.yml, integrate drizzle-orm and pg dependencies in invoices and orders services, and implement basic message broker setup for order processing.

--- orders/src/http/server.ts
+++ orders/src/http/server.ts
@@ -5,6 +5,9 @@ import {
   serializerCompiler,
   validatorCompiler,
  type  ZodTypeProvider } from "fastify-type-provider-zod";
+import { channels } from "../broker/channels/index.ts";
+import { schema } from "../db/schema/index.ts";
+import { client } from "../db/client.ts";
 
  
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -23,12 +26,22 @@ app.get("/health", () => {
 app.post("/orders", {
   schema: {
     body: z.object({
-      amount: z.number()
+      amount: z.coerce.number()
     }),
   },
-}, (request, reply) => {
+}, async (request, reply) => {
   const { amount } = request.body;
   console.log('New order received', amount);
+  const order = {
+    id: crypto.randomUUID(),
+    amount,
+    customerId: "my-customer-id", 
+  }
+    
+  await client.insert(schema.orders).values(order);
+  
+   channels.orders.sendToQueue("orders", Buffer.from(JSON.stringify(order)));
+
   return reply.status(201).send()
 })
 
